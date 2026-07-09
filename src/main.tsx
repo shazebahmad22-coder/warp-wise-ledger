7d646cf33e88b28ef60657816a7ecc153f9c7828
shazebahmad22-coder <shazebahmad22@gmail.com>
2026-07-09 21:43:48 +0530

    feat: add main client entry point

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./routes/__root";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("app")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

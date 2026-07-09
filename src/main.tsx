7937f1ec50fdc229ff9499b225afab7b86499acb
shazebahmad22-coder <shazebahmad22@gmail.com>
2026-07-09 21:44:20 +0530

    feat: add missing TanStack Start project files (server, routes, client entry)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./routes/__root";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from '@tanstack/react-router';
+import { router } from './routes/__root';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("app")!).render(
+ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
